ee9107c045a2028c754923d1fda9493087fdf94b
ipabitrasaikia1 <79037063+ipabitrasaikia1@users.noreply.github.com>
2021-11-20 17:41:09 +0530

    Creating REST API using Data from https://www.mockaroo.com/

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express")
+
+const app = express(); // creating server for us
+
+
+const data = require("./bookDetails.json")
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+
+    res.send(data)
+}) 
+
+app.get("/:author",(req,res)=>{
+
+    const newData = data.filter((userData)=> userData.author=== req.params.author )
+    res.send(newData)
+}) 
+
+app.post("/",(req,res)=>{
+    const newData = [...data,req.body]
+    res.send(newData)
+}) 
+
+app.patch("/:author",(req,res)=>{
+
+    console.log(req.params.author)
+    const newData = data.map((item)=>{
+        if(req.params.author === item.author){
+            console.log(req.body)
+
+            return req.body
+        }
+
+        return item
+    })
+
+    res.send(newData)
+}) 
+
+app.delete("/:author",(req,res)=>{
+    const newData  = data.filter((item) => item.author != req.params.author)
+    res.send(newData)
+})
+
+app.listen(2346,()=>{
+    console.log("PORT ready at 2346")
+})
